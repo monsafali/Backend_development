ffdb204224be581df3a96b34a15816d9871a5327
monsafali <monsafali272@gmail.com>
2024-06-21 08:43:59 +0500

    new challenge added

--- 01_Express.js/13_boss-level-challenge/app.js
+++ 01_Express.js/13_boss-level-challenge/app.js
@@ -55,6 +55,9 @@ app.post("/compose", function(req, res){
   res.redirect('/')
 });
 
+app.get("/posts/:postName", (req, res)=>{
+  console.log(req.params.postName)
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
