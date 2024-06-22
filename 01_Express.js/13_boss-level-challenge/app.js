f48807be4a6665999ba647cc58afebb83daa3455
monsafali <monsafali272@gmail.com>
2024-06-22 07:41:33 +0500

    new challenge added

--- 01_Express.js/13_boss-level-challenge/app.js
+++ 01_Express.js/13_boss-level-challenge/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -55,9 +56,23 @@ app.post("/compose", function(req, res){
   res.redirect('/')
 });
 
-app.get("/posts/:postName", (req, res)=>{
-  console.log(req.params.postName)
-})
+
+app.get("/posts/:postName", (req, res) => {
+  const requestedTitle = _.kebabCase(req.params.postName);
+  let found = false;
+  posts.forEach(post => {
+    const storedTitle = _.kebabCase(post.title);
+    if (storedTitle === requestedTitle) {
+      console.log("match found");
+      found = true;
+    }
+  });
+  if (!found) {
+    console.log("match not found");
+  }
+  res.redirect('/');
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
