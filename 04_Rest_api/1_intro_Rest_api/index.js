6a06db5f8b7f8f26ff92ca9ae5218f563f7d1e7d
monsafali <monsafali272@gmail.com>
2024-06-24 08:36:35 +0500

    api in ejs

--- 04_Rest_api/1_intro_Rest_api/index.js
+++ 04_Rest_api/1_intro_Rest_api/index.js
@@ -1,39 +1,35 @@
-const express = require("express");
+const express = require("express")
 const app = express();
 const port = 8080;
+
 const path = require("path")
+app.use(express.urlencoded({extended:true}))
 
-
-app.use(express.urlencoded({extended: true}))
-
-
-
-app.set("view enging", "ejs");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname,"public")))
 
 let posts = [
     {
-        username: "monsaf ali",
-        content: "i love coding"
+        username: "Apna Colle",
+        content: "I love Coding"
     },
     {
-        username: "muhammad kaleem",
-        content: "i love automation"
+        username: "Monsaf Ali",
+        content: "Only your work hard make you unique from others"
     },
     {
-        username: "kamal",
-        content: "i love Ai only you worked hard make you unique"
+        username: "Kaleem",
+        content: "Now one can achieve the desire golas a someone achieve them very work hard"
     },
-];
-
+]
 
 app.get("/posts", (req, res)=>{
-    // res.send("services working well!")
+    // res.send("server is start now")
     res.render("index.ejs", {posts})
 })
 
- app.listen(port, ()=>{
-    console.log("Listenign to port: 8080")
- })
+app.listen(port,()=>{
+    console.log("server is start at port", port)
+})
