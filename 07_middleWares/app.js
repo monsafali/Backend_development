c96b4d02093a295f1683bdc3cde4b8b197554621
monsafali <monsafali272@gmail.com>
2024-08-06 07:08:26 +0500

    express error handler

--- 07_middleWares/app.js
+++ 07_middleWares/app.js
@@ -4,25 +4,57 @@ const app = express();
 
 
 // app.use((req, res, next) => {
+//     console.log("I am only for random");
+//    next();
+// });
+
+// app.use((req, res, next) => {
 //     console.log("Hi, I am 1st middleware");
 //    return next();
 //    console.log('abc')   
 // });
+
 
 // app.use((req, res, next) => {
 //     console.log("Hi, I am 2nd middleware");
 //    next();
 // });
 
-app.use((req, res, next) => {
-    req.time = new Date(Date.now()).toString()
-    console.log(req.method, req.hostname,req.path,req.time)
-   next();
+const checkToken =  (req, res , next )=>{
+    let {token} = req. query;
+    if(token === "giveaccess"){
+        next();
+    }
+    throw new Error("Accesss Denied");
+};
+// app.get("/wrong", (req, res)=>{
+//     abcd = abcd;
+// })
+app.get("/api", checkToken,(req, res)=>{
+    res.send("data");
 });
 
 
+// LOgger - morgan
+// app.use((req, res, next) => {
+//     req.time = new Date(Date.now()).toString()
+//     console.log(req.method, req.hostname,req.path,req.time)
+//    next();
+// });
+
+app.use("/random", (req, res, next) =>{
+    console.log("I am only for a random")
+    next();
+})
+
+// 404
+// app.use((req, res) => {
+//     res.send("page Not found!")
+// })
+
+
 app.get("/", (req, res) =>{
-    res.send("Hi I'm Root ")
+    res.status(404).send("Hi I'm Root ")
 })
 
 app.get("/random", (req, res) =>{
