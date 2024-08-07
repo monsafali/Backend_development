322a2ff76f20bf686f228daf8b54ec1a0fd4c391
monsafali <monsafali272@gmail.com>
2024-08-07 21:18:11 +0500

    adding try & catch middle wear

--- 05_Data_base/04_MongoDB_with_Express/index.js
+++ 05_Data_base/04_MongoDB_with_Express/index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override")
+const ExpresError = require("./ExpressError")
+
 
 // Set up view engine and views directory
 app.set("views", path.join(__dirname, "views"));
@@ -21,42 +23,55 @@ main()
     .catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+    await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
 }
 
 
 // Indeex route
-app.get("/chats", async (req, res) => {
+app.get("/chats", async (req, res, next) => {
     try {
       let chats = await Chat.find();
       // console.log(chats); // Check structure here
       res.render("index", { chats });
     } catch (err) {
-      console.error(err);
-      res.status(500).send("Internal Server Error");
+      // console.error(err);
+      // res.status(500).send("Internal Server Error");
+      next(err);
     }
   });
   
   // New Route
   app.get("/chats/new", (req, res) =>{
+    // throw new ExpresError(404, "Page Not found");
     res.render("new")
   })
 
   // Create Route
-  app.post("/chats", (req, res)=>{
-    let {from, to, msg} = req.body;
-    let newChat  = new Chat ({
-      from: from,
-      to: to,
-      msg: msg,
-      created_at: new Date()
-    });
-   newChat.save().then(res =>{
-    console.log("Chat was saved successfuly ")
-  }).catch((err) =>{
-    console.log("Something went wrong with err:", err)
-  })
-    res.redirect("/chats");
+
+  app.post("/chats", async (req, res, next) => {
+    try{
+      let {from, to, msg} = req.body;
+      let newChat  = new Chat ({
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: new Date()
+      });
+      await newChat.save();
+      res.redirect("/chats");
+    }catch (err){
+      next(err)
+    }
+   });
+
+  // NEW Show Route
+  app.get("/chats/:id", async (req, res, next)=>{
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    // if(!chat) {
+    //   next(new ExpresError(404, "Chat Not found"));
+    // }
+    res.render("edit", {chat});
   })
 
 
@@ -98,6 +113,15 @@ app.delete("/chats/:id",  async(req, res)=>{
 app.get("/", (req, res) => {
     res.send("Root is working fine");
 });
+
+
+
+// Error Handlig Middleware
+app.use ((err, req, res, next)=>{
+  let {status=500, message = "Some Error Occurred"} = err;
+  res.status(status).send(message);
+})
+
 
 app.listen(port, () => {
     console.log("Server is listening on port:", port);
