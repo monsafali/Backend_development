a5453110846f4ba99f95dd289f1b84ec984ee629
monsafali <monsafali272@gmail.com>
2024-08-13 09:01:34 +0500

    review add on project

--- 06_Full_stack_mini_project/models/listing.js
+++ 06_Full_stack_mini_project/models/listing.js
@@ -1,37 +1,6 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-
-// const listingSchema = new Schema({
-//     title: {
-//         type: String,
-//         required: true
-//     },
-//     description: String,
-
-//     image: {
-//         type: String,
-//         default: "https://images.unsplash.com/photo-1721633617180-97c67428a48e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//         set: (v) => 
-//             v === "" ? "https://images.unsplash.com/photo-1721633617180-97c67428a48e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
-//     },
-    
-//     // image: {
-//     //     filename: String,
-//     //     url: String
-//     // },
-
-//     price: Number,
-//     location: String,
-//     country: String,
-// })
-
-
-// const Listing = mongoose.model("listing", listingSchema);
-// module.exports = Listing;
-
-
-
 const mongoose = require("mongoose");
+const review = require("./review");
+const { ref } = require("joi");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -51,20 +20,18 @@ const listingSchema = new Schema({
             default: "https://images.unsplash.com/photo-1721633617180-97c67428a48e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
     },
-
-    //   image: {
-    //     filename: String,
-    //     url: String
-    // },
-
-
-
     price: Number,
     location: String,
     country: {
         type: [String],
         default: []
-    }
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
 });
 
 const Listing = mongoose.model("listing", listingSchema);
