8cd6757a46c9f3746554c7814d154fd89e20e612
monsafali <monsafali272@gmail.com>
2024-08-17 09:41:31 +0500

    express state & session

--- 06_Full_stack_mini_project/clasroom/server.js
+++ 06_Full_stack_mini_project/clasroom/server.js
@@ -1,41 +1,29 @@
 const express = require("express")
 const app = express()
 const users = require("./routes/user")
+// const posts = require("./routes/post")
+const session = require('express-session')
 
-const cookieParser = require("cookie-parser")
 
+app.use(session({secret: "mysupersecretstring", resave: false, saveUninitialized: true}))
 
-app.use(cookieParser("secretcode"))
-
-app.get("/getsignedcookie", (req, res)=>{
-    res.cookie("made_in","Indian",{signed: true})
-    res.send("signed cokokies send")
+app.get("/reqcount", (req, res)=>{
+    if(req.session.count){
+        req.session.count ++
+    } else{
+        req.session.count = 1
+    }
+    
+    res.send(`You send a message ${req.session.count} time`);
 })
+// app.get("/test",(req, res) =>{
+//     res.send("text successfuly")
+// })
 
-app.get("/verify", (req, res)=>{
-    console.log(req.signedCookies)
-    res.send("verifiy")
-})
-
-app.get("/getcookies", (req, res)=>{
-    res.cookie("great", "Hello");
-    res.cookie("mission","Welcome to a big game")
-    res.send("Sent you some responsed")
-})
-
-app.get("/greet", (req, res)=>{
-    let {name = "Kamal"} = req.cookies 
-    res.send(`Hi ${name}`)
-})
-
-app.get("/", (req, res)=>{
-    console.dir(req.cookies)
-    res.send("Hi I am root!")
-})
-
-app.use("/users", users)
 
 
 app.listen(3000, ()=>{
     console.log("server start successfully")
 })
+
+
