fcc5b75c15853caf25ed996a80209cf3edc3cc92
monsafali <monsafali272@gmail.com>
2024-08-19 08:31:01 +0500

    authentication & autorization

--- 06_Full_stack_mini_project/models/review.js
+++ 06_Full_stack_mini_project/models/review.js
@@ -1,23 +1,3 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-
-// const reviewSchema = new Schema ({
-//     Comment: String,
-//     rating: {
-//         type : Number,
-//         min: 1,
-//         max: 5
-//     },
-//     createdAt: {
-//         type: Date,
-//         default: Date.now()
-//     }
-// });
-
-// module.exports = mongoose.model("Review", reviewSchema)
-
-
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
